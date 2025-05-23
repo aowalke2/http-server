2052df0640a3db65cd395e87a7e8d56e5cd0b877
Aza Walker <azawalker.jr@gmail.com>
2025-05-23 16:12:12 -0400

    returning a response

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,31 @@
-use std::io::Write;
-#[allow(unused_imports)]
-use std::net::TcpListener;
+use std::io::{BufRead, BufReader, Write};
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                stream
-                    .write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
-                    .unwrap();
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+            Ok(stream) => handle_connection(stream),
+            Err(e) => println!("error: {}", e),
         }
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let http_request = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect::<Vec<String>>();
+
+    let request_line = http_request[0].as_str().split(" ").collect::<Vec<&str>>();
+    let route = request_line[1];
+    let response = match route {
+        "/" => "HTTP/1.1 200 OK\r\n\r\n",
+        _ => "HTTP/1.1 404 Not Found\r\n\r\n",
+    };
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
