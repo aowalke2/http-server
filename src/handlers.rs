e17a20c50c726d883fb0c71476d462c69882ab47
Aza Walker <azawalker.jr@gmail.com>
2025-05-28 16:10:17 -0400

    handling encodings

--- src/handlers.rs
+++ src/handlers.rs
@@ -12,6 +12,8 @@ use crate::{
     response::*,
 };
 
+const COMPRESSION_SCHEMES: [&str; 1] = ["gzip"];
+
 pub fn handle_connection(mut stream: TcpStream) {
     let http_request = Request::new(&mut stream);
     let route = http_request.route();
@@ -19,24 +21,45 @@ pub fn handle_connection(mut stream: TcpStream) {
     //println!("{}", &http_request.content_type());
 
     let response = match (method, route[0].as_str()) {
-        (HttpMethod::Get, "echo") => handle_echo(route[1].clone()),
+        (HttpMethod::Get, "echo") => handle_echo(route[1].clone(), &http_request.headers()),
         (HttpMethod::Get, "user-agent") => handle_user_agent(&http_request.headers()),
         (HttpMethod::Get, "files") => handle_read_file(route[1].clone()),
         (HttpMethod::Post, "files") => handle_create_file(route[1].clone(), http_request.body()),
-        (HttpMethod::Get, "") => ok(),
-        _ => not_found(),
+        (HttpMethod::Get, "") => Response::new(StatusCode::Ok).build(),
+        _ => Response::new(StatusCode::NotFound).build(),
     };
 
     stream.write_all(response.as_bytes()).unwrap();
 }
 
-fn handle_echo(content: String) -> String {
-    ok_with_content("text/plain", &content)
+fn handle_echo(content: String, headers: &HashMap<String, String>) -> String {
+    let response = Response::new(StatusCode::Ok)
+        .with_header("Content-Type", "text/plain")
+        .with_header("Content-Length", &content.chars().count().to_string())
+        .with_body(&content);
+
+    if let Some(encodings) = headers.get("Accept-Encoding") {
+        let mut valid_encodings = Vec::new();
+        for encoding in encodings.split(", ") {
+            if COMPRESSION_SCHEMES.contains(&encoding) {
+                valid_encodings.push(encoding);
+            }
+        }
+        response
+            .with_header("Content-Encoding", valid_encodings.join(", ").as_str())
+            .build()
+    } else {
+        response.build()
+    }
 }
 
 fn handle_user_agent(headers: &HashMap<String, String>) -> String {
     let content = headers.get("User-Agent").unwrap();
-    ok_with_content("text/plain", content)
+    Response::new(StatusCode::Ok)
+        .with_header("Content-Type", "text/plain")
+        .with_header("Content-Length", &content.chars().count().to_string())
+        .with_body(&content)
+        .build()
 }
 
 fn handle_read_file(file: String) -> String {
@@ -45,12 +68,16 @@ fn handle_read_file(file: String) -> String {
             Ok(mut file) => {
                 let mut content = String::new();
                 file.read_to_string(&mut content).unwrap();
-                ok_with_content("application/octet-stream", &content)
+                Response::new(StatusCode::Ok)
+                    .with_header("Content-Type", "application/octet-stream")
+                    .with_header("Content-Length", &content.chars().count().to_string())
+                    .with_body(&content)
+                    .build()
             }
-            Err(_) => not_found(),
+            Err(_) => Response::new(StatusCode::NotFound).build(),
         }
     } else {
-        not_found()
+        Response::new(StatusCode::NotFound).build()
     }
 }
 
@@ -59,11 +86,11 @@ fn handle_create_file(file: String, content: String) -> String {
         match File::create(Path::new(&dir).join(file)) {
             Ok(mut file) => {
                 file.write_all(content.as_bytes()).unwrap();
-                created()
+                Response::new(StatusCode::Created).build()
             }
-            Err(_) => not_found(),
+            Err(_) => Response::new(StatusCode::NotFound).build(),
         }
     } else {
-        not_found()
+        Response::new(StatusCode::NotFound).build()
     }
 }
--- src/response.rs
+++ src/response.rs
@@ -1,20 +1,54 @@
-pub fn not_found() -> String {
-    "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub enum StatusCode {
+    NotFound,
+    Ok,
+    Created,
 }
 
-pub fn ok() -> String {
-    "HTTP/1.1 200 OK\r\n\r\n".to_string()
+impl From<StatusCode> for String {
+    fn from(status: StatusCode) -> Self {
+        match status {
+            StatusCode::Ok => "HTTP/1.1 200 OK\r\n".to_string(),
+            StatusCode::Created => "HTTP/1.1 201 Created\r\n".to_string(),
+            StatusCode::NotFound => "HTTP/1.1 404 Not Found\r\n".to_string(),
+        }
+    }
 }
 
-pub fn ok_with_content(content_type: &str, content: &str) -> String {
-    format!(
-        "HTTP/1.1 200 OK\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n{}",
-        content_type,
-        content.chars().count(),
-        content
-    )
+pub struct Response {
+    status: StatusCode,
+    headers: HashMap<String, String>,
+    body: String,
 }
 
-pub fn created() -> String {
-    "HTTP/1.1 201 Created\r\n\r\n".to_string()
+impl Response {
+    pub fn new(status: StatusCode) -> Self {
+        Response {
+            status,
+            headers: HashMap::new(),
+            body: String::new(),
+        }
+    }
+
+    pub fn with_header(mut self, key: &str, value: &str) -> Self {
+        self.headers.insert(key.to_string(), value.to_string());
+        self
+    }
+
+    pub fn with_body(mut self, body: &str) -> Self {
+        self.body = body.to_string();
+        self
+    }
+
+    pub fn build(self) -> String {
+        let mut response = String::from(self.status);
+        for (key, value) in self.headers {
+            response.push_str(&format!("{}: {}\r\n", key, value));
+        }
+        response.push_str("\r\n");
+        response.push_str(&self.body);
+        response
+    }
 }
